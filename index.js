a9ece03d4329a983e0ad272bd9010e7eaf1186be
Gregory Banks <gbanks1438@gmail.com>
2021-11-18 17:41:24 -0500

    Final product

--- index.js
+++ index.js
@@ -1,51 +1,89 @@
-// console.log("Hi")
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("The DOM is ready to play!")
-})
-
 const flataDriversURL = 'https://mario-kart-tour-api.herokuapp.com/api/v1/drivers'
-
-fetch(flataDriversURL)
-.then(response => response.json())
-.then(kartData => { 
-  // console.log(kartData[0].name)   
-
-  const characterDetails = document.querySelector("#player-details")
-
-    const marioName = kartData[0].name
-    const peachName = kartData[1].name
-    const daisyName = kartData[2].name
-
-    const characterOne = document.querySelector("#character-1")
-      characterOne.textContent = marioName
-      characterDetails.append(characterOne)
-
-      const characterTwo = document.querySelector("#character-2")
-      characterTwo.textContent = peachName
-      characterDetails.append(characterTwo)
-
-      const characterThree = document.querySelector("#character-3")
-      characterThree.textContent = daisyName
-      characterDetails.append(characterThree)
-});
-
-// -- header mouseenter event
-const headerSection = document.querySelector("#flata-header")
-
-headerSection.addEventListener("mouseenter" , () => {
-  // console.log("Mouse cursor detected!") 
-  headerSection.style.border = '5px dotted orange';
-})
-
-headerSection.addEventListener("mouseleave", () => {
-  // console.log("Mouse cursor has left!") 
-  headerSection.style.border = '5px dotted blue';
-} )
-
-// -- submit event
+const characterDetails = document.querySelector("#player-details")
+const characterOne = document.querySelector("#character-1")
+const characterTwo = document.querySelector("#character-2")
+const characterThree = document.querySelector("#character-3")
 const submitBtn = document.querySelector("#submit-button")
 
-submitBtn.addEventListener("click", () => {
-  // console.log("click work!")
-  alert("Yiiiiiipeee! Let's-a go! 🏁 🏎️ 🏎️ 🏎️ 🏁");
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("The DOM is ready to play!")
+
+  fetch(flataDriversURL)
+    .then(response => response.json())
+    .then(kartData => {
+      // console.log(kartData)  // gives the array of all objects
+      kartData.forEach(character => {
+        // console.log(character.name)  // gives all names
+
+        if (character.position === 1) {
+          const marioName = character.name
+          const marioSpecialSkill = character.special_skill
+          const marioTour = character.debut_tour
+
+          const characterOne = document.querySelector("#character-1")
+
+          const divLiListOne = document.createElement("p")
+          const divLiListTwo = document.createElement("p")
+          const divLiListThree = document.createElement("p")
+
+          divLiListOne.textContent = marioName
+          divLiListTwo.textContent = `Special Skill: ${marioSpecialSkill}`
+          divLiListThree.textContent = `Tour: ${marioTour}`
+
+          characterOne.append(divLiListOne, divLiListTwo, divLiListThree)
+        }
+        else if (character.position === 2) {
+          const peachName = character.name
+          const peachSpecialSkill = character.special_skill
+          const peachTour = character.debut_tour
+
+          const characterTwo = document.querySelector("#character-2")
+
+          const divLiListOneForPeach = document.createElement("p")
+          const divLiListTwoForPeach = document.createElement("p")
+          const divLiListThreeForPeach = document.createElement("p")
+
+          divLiListOneForPeach.textContent = peachName
+          divLiListTwoForPeach.textContent = `Special Skill: ${peachSpecialSkill}`
+          divLiListThreeForPeach.textContent = `Tour: ${peachTour}`
+
+          characterTwo.append(divLiListOneForPeach, divLiListTwoForPeach, divLiListThreeForPeach)
+        }
+        else if (character.position === 3) {
+          const daisyName = character.name
+          const daisySpecialSkill = character.special_skill
+          const daisyTour = character.debut_tour
+
+          const characterThree = document.querySelector("#character-3")
+
+          const divLiListOneForDaisy = document.createElement("p")
+          const divLiListTwoForDaisy = document.createElement("p")
+          const divLiListThreeForDaisy = document.createElement("p")
+
+          divLiListOneForDaisy.textContent = daisyName
+          divLiListTwoForDaisy.textContent = `Special Skill: ${daisySpecialSkill}`
+          divLiListThreeForDaisy.textContent = `Tour: ${daisyTour}`
+
+          characterThree.append(divLiListOneForDaisy, divLiListTwoForDaisy, divLiListThreeForDaisy)
+        }
+      });
+
+      // -- header mouseenter event
+      const headerSection = document.querySelector("#flata-header")
+
+      headerSection.addEventListener("mouseenter", () => {
+        // console.log("Mouse cursor detected!") 
+        headerSection.style.border = '5px dotted orange';
+      })
+
+      headerSection.addEventListener("mouseleave", () => {
+        // console.log("Mouse cursor has left!") 
+        headerSection.style.border = '5px dotted blue';
+      })
+    })
+
+  submitBtn.addEventListener("click", () => {
+    // console.log("click work!")
+    alert("Yiiiiiipeee! Let's-a go! 🏁 🏎️ 🏎️ 🏎️ 🏁");
+  })
 })
